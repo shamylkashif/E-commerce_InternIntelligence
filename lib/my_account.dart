b586db9552d4a3735c0996bc32fb0175ac9fe992
shamylkashif <144819710+shamylkashif@users.noreply.github.com>
2024-09-24 12:10:32 +0500

    commit and push

--- lib/my_account.dart
+++ lib/my_account.dart
@@ -16,167 +16,190 @@ class _MyProfileState extends State<MyProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-         appBar: AppBar(
-           automaticallyImplyLeading: false,
-           actions: [
-             TextButton(
-                 onPressed: (){
-                   Navigator.push(context, MaterialPageRoute(builder: (context)=> LoginScreen()));
-                 },
-                 child: Container(
-                    height: 33,
-                    width: 75,
-                    decoration: BoxDecoration(
-                      color: yellow,
-                      borderRadius: BorderRadius.circular(15)
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          actions: [
+            TextButton(
+                onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> LoginScreen()));
+                },
+                child: Container(
+                   height: 33,
+                   width: 75,
+                   decoration: BoxDecoration(
+                     color: yellow,
+                     borderRadius: BorderRadius.circular(15)
+                   ),
+                  child: Center(child: Text('LogOut', style: TextStyle(color: blue, fontSize: 16),)),
+                ),
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+           child: Column(
+            children: [
+              SizedBox(height: 10,),
+              Center(
+                child: Container(
+                  height: 180,
+                  width: 300,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                      yellow, blue
+                    ],
+                      begin: const FractionalOffset(0.5, 1),
+                      end: const FractionalOffset(1.0, 0.0),
                     ),
-                   child: Center(child: Text('LogOut', style: TextStyle(color: blue, fontSize: 16),)),
-                 ),
-             ),
-           ],
-         ),
-         body: Column(
-           children: [
-             SizedBox(height: 15,),
-             Center(
-               child: Container(
-                 height: 180,
-                 width: 300,
-                 decoration: BoxDecoration(
-                   gradient: LinearGradient(colors: [
-                     yellow, blue
-                   ],
-                     begin: const FractionalOffset(0.5, 1),
-                     end: const FractionalOffset(1.0, 0.0),
-                   ),
-                   borderRadius: BorderRadius.circular(25),
+                    borderRadius: BorderRadius.circular(25),
 
-                 ),
-                 child: Column(
-                   children: [
-                      SizedBox(height: 16,),
-                     Stack(
-                       children: [
-                         Center(
-                           child: Container(
-                             height: 80,
-                             width: 80,
-                             decoration: BoxDecoration(
-                                 borderRadius: BorderRadius.all(Radius.circular(100)),
-                                 image: DecorationImage(image: AssetImage('assets/p.jpg'))
-                             ),
-                           ),
-                         ),
-                         Container(
-                           margin: EdgeInsets.only(top: 70,left: 137),
-                           height:30,
-                           width: 30,
-                           decoration: BoxDecoration(
-                               color: Colors.white,
-                               shape: BoxShape.circle
-                           ),
-                           child: InkWell(
-                               onTap: (){
-                                 showModalBottomSheet(
-                                     context: context,
-                                     shape: RoundedRectangleBorder(
-                                       borderRadius: BorderRadius.vertical(
-                                         top: Radius.circular(15),
-                                         bottom: Radius.circular(15)
-                                       ),
-                                     ),
-                                     builder: (context){
-                                       return ProfilePicChoice();
-                                     }
-                                 );                                 },
-                               child: Icon(Icons.camera_alt_outlined, color: blue, size: 20,)),
-                         ),
-                       ],
-                     ),
+                  ),
+                  child: Column(
+                    children: [
+                       SizedBox(height: 16,),
+                      Stack(
+                        children: [
+                          Center(
+                            child: Container(
+                              height: 80,
+                              width: 80,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(Radius.circular(100)),
+                                  image: DecorationImage(image: AssetImage('assets/p.jpg'))
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 70,left: 137),
+                            height:30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                shape: BoxShape.circle
+                            ),
+                            child: InkWell(
+                                onTap: (){
+                                  showModalBottomSheet(
+                                      context: context,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.vertical(
+                                          top: Radius.circular(15),
+                                          bottom: Radius.circular(15)
+                                        ),
+                                      ),
+                                      builder: (context){
+                                        return ProfilePicChoice();
+                                      }
+                                  );                                 },
+                                child: Icon(Icons.camera_alt_outlined, color: blue, size: 20,)),
+                          ),
+                        ],
+                      ),
 
-                     SizedBox(height: 8,),
-                     Text('Clara Albert',
-                       style: TextStyle(
-                           fontSize: 20,
-                           color: Colors.white,
-                           height: 1.0
-                       ),),
-                     Text('ClaraAlbert@gmail.com',
-                       style: TextStyle(
-                           fontSize: 16,
-                           color: Colors.white,
-                           height: 1.0
-                       ),),
-                   ],
-                 ),
-               ),
-             ),
-              SizedBox(height: 20,),
-             Column(
-               children: [
-                 ListTile(
-                   leading:Icon(Icons.person, color: blue,) ,
-                   title: Text(
-                     'Username', style: TextStyle(fontSize: 14, ),
-                   ),
-                   subtitle: Text(
-                     'Clara Albert', style: TextStyle(fontSize: 17,),
-                   ),
-                   trailing: InkWell(
-                       onTap: (){},
-                       child: Icon(Icons.edit, color: blue,)),
-                 ),
-                 ListTile(
-                   leading:Icon(Icons.phone_android, color: blue,) ,
-                   title: Text(
-                     'Mobile Number', style: TextStyle(fontSize: 14, ),
-                   ),
-                   subtitle: Text(
-                     '03XZ-YYYYYYY', style: TextStyle(fontSize: 17,),
-                   ),
-                   trailing: InkWell(
-                       onTap: (){},
-                       child: Icon(Icons.edit, color: blue,)),
-                 ),
-                 ListTile(
-                   leading:Icon(Icons.email_outlined, color: blue,) ,
-                   title: Text(
-                     'Email Address', style: TextStyle(fontSize: 14, ),
-                   ),
-                   subtitle: Text(
-                     'ClaraAlbert@gmail.com', style: TextStyle(fontSize: 17,),
-                   ),
-                   trailing: InkWell(
-                       onTap: (){},
-                       child: Icon(Icons.edit, color: blue,)),
-                 ),
-                 ListTile(
-                   leading:Icon(Icons.location_on, color: blue,) ,
-                   title: Text(
-                     'Location', style: TextStyle(fontSize: 14, ),
-                   ),
-                   subtitle: Text(
-                     'Islamabad', style: TextStyle(fontSize: 17,),
-                   ),
-                   trailing: InkWell(
-                       onTap: (){},
-                       child: Icon(Icons.edit, color: blue,)),
-                 ),
-                 ListTile(
-                   leading:Icon(Icons.settings, color: blue,) ,
-                   title: Text(
-                     'Settings', style: TextStyle(fontSize: 14, ),
-                   ),
-                   trailing: InkWell(
-                       onTap: (){
-                         Navigator.push(context, MaterialPageRoute(builder: (context)=>Settings()));
-                       },
-                       child: Icon(Icons.edit, color: blue,)),
-                 ),
-               ],
-             ),
-           ],
-         ),
+                      SizedBox(height: 8,),
+                      Text('Clara Albert',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.white,
+                            height: 1.0
+                        ),),
+                      Text('ClaraAlbert@gmail.com',
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            height: 1.0
+                        ),),
+                    ],
+                  ),
+                ),
+              ),
+               SizedBox(height: 20,),
+              Column(
+                children: [
+                  EditableProfileTile(
+                      title: 'Username',
+                      value: 'Clara Albert',
+                      icons: Icons.person),
+                  EditableProfileTile(
+                      title: 'Mobile Number',
+                      value: '03XZ-YYYYYYY',
+                      icons: Icons.phone_android),
+                  EditableProfileTile(
+                      title: 'Email Address',
+                      value: 'claraalbert596@gmail.com',
+                      icons: Icons.email),
+                  EditableProfileTile(
+                      title: 'Location',
+                      value: 'Islamad',
+                      icons: Icons.location_on),
+                  ListTile(
+                    leading:Icon(Icons.settings, color: blue,) ,
+                    title: Text(
+                      'Settings', style: TextStyle(fontSize: 14, ),
+                    ),
+                    trailing: InkWell(
+                        onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>Settings()));
+                        },
+                        child: Icon(Icons.edit, color: blue,)),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+          );
+  }
+}
+
+
+class EditableProfileTile extends StatefulWidget {
+  final String title;
+  final String value;
+  final IconData icons;
+
+  const EditableProfileTile({
+    Key? key,
+    required this.title,
+    required this.value,
+    required this.icons,
+  }) : super(key: key)  ;
+
+  @override
+  State<EditableProfileTile> createState() => _EditableProfileTileState();
+}
+
+class _EditableProfileTileState extends State<EditableProfileTile> {
+
+  bool isEditing = false;
+  late TextEditingController _controller;
+  @override
+
+  void initState(){
+    super.initState();
+    _controller = TextEditingController(text: widget.value);
+  }
+
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(widget.icons, color: blue),
+      title: Text(widget.title, style: TextStyle(fontSize: 14)),
+      subtitle: isEditing
+          ? TextField(
+        controller: _controller,
+        autofocus: true, // Automatically focus on the text field
+        decoration: InputDecoration(hintText: 'Enter ${widget.title}'),
+      )
+          : Text(_controller.text, style: TextStyle(fontSize: 17)),
+      trailing: IconButton(
+        icon: Icon(isEditing ? Icons.check : Icons.edit, color: blue),
+        onPressed: () {
+          setState(() {
+            isEditing = !isEditing;
+          });
+        },
+      ),
     );
   }
 }
+
+

136ae63a9de42972894e4c238cdfc48387978c77
shamylkashif <144819710+shamylkashif@users.noreply.github.com>
2024-10-27 17:34:25 +0500

    commit and push

--- lib/forgotpassword.dart
+++ lib/forgotpassword.dart
@@ -1,6 +1,8 @@
+import 'package:bookstore/password_verification.dart';
+import 'package:bookstore/screens/signup-screen.dart';
 import 'package:flutter/material.dart';
 import 'package:bookstore/commons/colors.dart';
-import 'package:bookstore/reset.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ForgotPassword extends StatefulWidget {
   const ForgotPassword({super.key});
@@ -12,54 +14,133 @@ class ForgotPassword extends StatefulWidget {
 class _ForgotPasswordState extends State<ForgotPassword> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(bottom: 150,top: 40 ),
-      decoration: BoxDecoration(
-        color: yellow,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(20.0),
-          topRight: Radius.circular(20.0),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        leading: InkWell(
+            onTap: (){
+              Navigator.pop(context);
+            },
+            child: Icon(Icons.arrow_back_ios,color: blue,)),
+        title: Text('Forgot Password', ),
+        centerTitle: true,
       ),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            'Make Selection!',
-            style: TextStyle(color: blue, fontSize: 18.0),
-          ),
-          SizedBox(height: 10.0),
-          ListTile(
-            leading: Icon(Icons.email, color: blue),
-            title: Text(
-              'E-Mail',
-              style: TextStyle(color: blue),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 50,),
+            Text('Enter Email Address registered with this platform', ),
+            SizedBox(height: 20,),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(28),
+                  ),
+                  labelText: 'abc@gmail.com',
+                  labelStyle: const TextStyle(color: Colors.grey),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                      borderRadius: BorderRadius.circular(28)
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                        borderRadius: BorderRadius.circular(28)
+                    )
+                ),
+              ),
             ),
-            subtitle: Text(
-              'Reset via Mail Verification',
-              style: TextStyle(color: blue),
+            SizedBox(height: 18,),
+            InkWell(
+                onTap: (){
+                  Navigator.pop(context);
+                },
+                child: Text('Back to Sign in')),
+            SizedBox(height: 35,),
+            InkWell(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>PasswordVerification()));
+              },
+              child: Container(
+                height: 55,
+                width: 310,
+                decoration: BoxDecoration(
+                  color: yellow,
+                  borderRadius: BorderRadius.horizontal(right: Radius.circular(28), left: Radius.circular(28))
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: Text('Send',
+                    style: TextStyle(color: blue,fontSize: 18),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
             ),
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>  Reset())
-              );
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.phone, color: blue),
-            title: Text(
-              'Phone No',
-              style: TextStyle(color: blue),
+            SizedBox(height: 70,),
+            Text('or', style: TextStyle(fontSize: 18),),
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              ShaderMask(
+                shaderCallback: (Rect bounds) {
+                  return LinearGradient(
+                    colors: [
+                      Color(0xFFEA4335), // Google Red
+                      Color(0xFFFBBC05), // Google Yellow
+                      Color(0xFF34A853), // Google Green
+                      Color(0xFF4285F4), // Google Blue
+                    ],
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomRight,
+                  ).createShader(bounds);
+                },
+                child: InkWell(
+                  onTap: (){
+
+                  },
+                  child: FaIcon(
+                    FontAwesomeIcons.google,
+                    size: 30,
+                    color: Colors.white, // Base color for gradient
+                  ),
+                ),
+              ),
+              SizedBox(width: 25,),
+              InkWell(
+                  onTap: (){
+
+                  },
+                  child: FaIcon(FontAwesomeIcons.facebook, color: Colors.blue, size: 30,))
+
+            ],),
+            SizedBox(height: 70,),
+            Text('Don\'t have an Account'),
+            SizedBox(height: 15,),
+            InkWell(
+              onTap: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>SignupScreen()));
+              },
+              child: Container(
+                height: 55,
+                width: 310,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.horizontal(right: Radius.circular(28), left: Radius.circular(28))
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(14.0),
+                  child: Text('Sign up',
+                    style: TextStyle(color: blue,fontSize: 18),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
             ),
-            subtitle: Text(
-              'Reset via Phone Verification',
-              style: TextStyle(color: blue),
-            ),
-            onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context)=> Reset())
-              );
-            },
-          ),
-        ],
+
+          ],
+        ),
       ),
     );
   }
---
+++ lib/password_verification.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class PasswordVerification extends StatefulWidget {
+  const PasswordVerification({super.key});
+
+  @override
+  State<PasswordVerification> createState() => _PasswordVerificationState();
+}
+
+class _PasswordVerificationState extends State<PasswordVerification> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
